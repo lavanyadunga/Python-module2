6c4236542a3673b47d289a8c157fad9dce800b01
Lavanya Dunga <lavanya.dunga@gmail.com>
2026-06-02 15:57:16 -0400

    lab of types and varibles

---
+++ types_and_vars.py
@@ -0,0 +1,22 @@
+name="lavanya" 
+age= 20
+height= 5.5
+print ("Hello,I am", name, "my age is", age, "and my height is", height)
+print("Hello,I am " + name + ", my age is " + str(age) + " and my height is " + str(height))
+x=10
+y=1
+z=0
+print(x>y)
+text="pxypxypxy"
+for letter in text:
+    if letter=="x":
+        continue
+    print(letter,end="")
+
+
+
+
+
+
+
+
