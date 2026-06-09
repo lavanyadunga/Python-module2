dcd3333b166c433465ce00a35c62347718f1eca1
Lavanya Dunga <lavanya.dunga@gmail.com>
2026-06-09 16:49:54 -0400

    lab for types and vars

--- types_and_vars.py
+++ types_and_vars.py
@@ -1,22 +1,37 @@
-name="lavanya" 
-age= 20
-height= 5.5
-print ("Hello,I am", name, "my age is", age, "and my height is", height)
-print("Hello,I am " + name + ", my age is " + str(age) + " and my height is " + str(height))
-x=10
-y=1
-z=0
-print(x>y)
-text="pxypxypxy"
-for letter in text:
-    if letter=="x":
-        continue
-    print(letter,end="")
 
+# Basic personal information
+name = "lavanya"
+age = 20
+height = 5.5
 
+# Introduction sentence using string concatenation
+print("Hello, I am " + name + ", my age is " + str(age) + " and my height is " + str(height))
 
+# -----------------------------------------------------------------------------
+# Calculate age in 5 years
+# This shows use of the addition operator (+)
+age_in_5_years = age + 5
+print("In 5 years, I will be " + str(age_in_5_years) + " years old.")
 
+# -----------------------------------------------------------------------------
+# Calculate the area of a rectangle (width x height)
+# Demonstrates multiplication (*) operator
+width = 5.5
+height_rect = 2
+area = width * height_rect
+print(f"The area of a {width} x {height_rect} rectangle is {area}.")
 
+# -----------------------------------------------------------------------------
+# Additional arithmetic demonstrations
+# Integer division (//) and modulus (%) examples
+div_example = 7 // 2   # integer division -> 3
+mod_example = 7 % 2    # remainder -> 1
+print("7 // 2 = " + str(div_example) + " (integer division)")
+print("7 % 2 = " + str(mod_example) + " (modulus/remainder)")
+
+# -----------------------------------------------------------------------------
+# String repetition example (repeat a separator line)
+print("-" * 30)
 
 
 
